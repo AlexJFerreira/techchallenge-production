a34c203cf5bff8df72a22ffb5db4bc82ce5bd045
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-11 20:21:48 -0300

    initial commit

---
+++ src/main/java/br/com/postech/techchallengeproduction/TechchallengeProductionApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallengeproduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeProductionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechchallengeProductionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/postech/techchallengeproduction/TechchallengeProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallengeproduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeProductionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
